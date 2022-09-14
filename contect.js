c9def62070f82f25f3172cb6f39d7d45234030a9
pavan <vermapavan07@gmail.com>
2022-09-14 23:16:34 +0530

    Second last update

--- contect.js
+++ contect.js
@@ -1,1 +1,26 @@
-const fn = document.getElementById();
+const form = document.getElementById("sheetdb-form")
+
+//submit from data
+form.addEventListener("submit", e => {
+    e.preventDefault();
+    if(1)
+    {
+        fetch(form.action, {
+            method: "POST",
+            body: new FormData(document.getElementById("sheetdb-form"))
+        }).then(
+            Response => Response.json()
+        ).then(alert("your responce submit successfully! We will contect you leter"))
+       clearForm();
+    }
+    else{
+        alert("Input Data in not correct.")
+    }
+})
+
+//Clear form once submitted
+function clearForm(){
+    form.reset();
+}
+
+
